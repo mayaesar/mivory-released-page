7050c983444beb6cb327813a06e8901595647712
Maya <mayaesar@gmail.com>
2025-04-09 18:51:54 -0400

    feat: hero Information

---
+++ components/HeroInfo.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <h1 class="text-4xl font-bold">People forget, <br/> Mivory doesn't</h1>
+    <h3 class="text-xl">Save, Organize, Collaborate</h3>
+    <div class="flex flex-row items-center gap-4 mt-4">
+      <UButton color="neutral" size="lg" icon="ph:app-store-logo">
+        App Store
+      </UButton>
+      <UButton color="neutral" size="lg" icon="ph:google-play-logo">
+        Google Play
+      </UButton>
+    </div>
+    <div class="flex flex-row items-center gap-4 mt-4">
+      <div>
+        <UAvatarGroup>
+          <UAvatar src="https://github.com/benjamincanac.png" alt="Benjamin Canac" />
+          <UAvatar src="https://github.com/romhml.png" alt="Romain Hamel" />
+          <UAvatar src="https://github.com/noook.png" alt="Neil Richter" />
+        </UAvatarGroup>
+      </div>
+      <div class="flex flex-col gap-1">
+        <div class="flex flex-row items-center gap-0.5">
+          <UIcon name="ph:star-fill" class="text-yellow-500" size="24" />
+          <UIcon name="ph:star-fill" class="text-yellow-500" size="24" />
+          <UIcon name="ph:star-fill" class="text-yellow-500" size="24" />
+          <UIcon name="ph:star-fill" class="text-yellow-500" size="24" />
+          <UIcon name="ph:star-fill" class="text-yellow-500" size="24" />
+        </div>
+        253 Users saving in Mivory
+      </div>
+    </div>
+  </div>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,10 @@
 <template>
   <div class="font-gray-900">
     <TopNav />
-    <div class="h-dvh max-w-dvw flex flex-col gap-3 py-3">
-
+    <div class="h-dvh max-w-dvw flex flex-col gap-4 px-6 py-6">
+      <div class="flex flex-row items-center justify-between">
+        <HeroInfo />
+      </div>
     </div>
   </div>
 
